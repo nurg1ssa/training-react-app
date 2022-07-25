46157306a4667ed46f6047e94abd4c09f4d809fc
nurg1ssa <nurike2001@mail.ru>
2022-07-25 22:37:20 +0600

    routing added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Blog from './Pages/Blog';
 
 function App() {
   return (
     <div>
-      <Header></Header>
+      <BrowserRouter>
+        <div className='app-wraper'>
+          <Header />
+          <div className='app-wraper-content'>
+            <Routes>
+              <Route exact path='/' element={<Home />} />
+              <Route exact path='/about' element={<About />} />
+              <Route exact path='/contacts' element={<Contact />} />
+              <Route exact path='/blog' element={<Blog />} />
+            </Routes>
+          </div>
+        </div>
+
+      </BrowserRouter>
     </div>
   );
 }
