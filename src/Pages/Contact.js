a41b24fbc4ec650783e57b6b64ae3394e05d4ab3
nurg1ssa <nurike2001@mail.ru>
2022-07-31 19:11:27 +0600

    contact us form added

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
+import { Button, Container, Form } from 'react-bootstrap'
 
 export default class Contact extends Component {
   render() {
     return (
-      <div>Contact</div>
+      <Container style={{width:500}}>
+        <h1 className='text-center'>Contact Us</h1>
+        <Form>
+          <Form.Group controlId='formBasicEmail'>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type='email' placeholder='Enter email'></Form.Control>
+          </Form.Group>
+          <Form.Group controlId='formBasicPassword'>
+          <Form.Label>Example textarea</Form.Label>
+            <Form.Control as='textarea' rows='3' placeholder='Enter email'></Form.Control>
+            </Form.Group>
+            <Form.Group controlId='formBasicCheckBox'>
+          <Form.Label>Example textarea</Form.Label>
+            <Form.Check type='checkbox' label='Check me out'></Form.Check>
+            </Form.Group>
+            <Button variant='primary' type='submit'>Submit</Button>
+        </Form>
+      </Container>
     )
   }
 }
